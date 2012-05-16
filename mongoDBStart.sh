0516dc2fb4cfab4bd20d310174a4fa8691597a0e
Rui Bando <bando.rui@gmail.com>
2012-05-17 00:01:06 +0900

    make mongoDBStart.sh

---
+++ mongoDBStart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DATA=/Users/ruibando/Mongo/mongodb_data/
+if [ -f $DATA ]
+then
+    echo "Connect MongoDB"
+    mongod --dbpath=/Users/ruibando/Mongo/mongodb_data/
+elif [ ! -f $DATA ]
+then
+    echo "Make directry for save MongoDB data"
+    mkdir -p $DATA
+    echo "Connect MongoDB"
+    mongod --dbpath=/Users/ruibando/Mongo/mongodb_data/
+fi
