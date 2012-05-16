c32e1a5781cfb48b9a9db443555cfe1c123040c9
Rui Bando <bando.rui@gmail.com>
2012-05-16 23:25:28 +0900

    make select and query

--- query.js
+++ query.js
@@ -1,2 +1,10 @@
-var query = {};
-db.test.find(query).limit(10).forEach(function(x){ print("name"); });
+//get explain
+var explain = db.test.find().explain();
+printjson(explain);
+
+var query = { created_on : ISODate("2012-05-15T15:22:02.853Z") };
+//db.test.find(query).limit(10).forEach(function(){ print("name"); } );
+db.test.find(query).forEach(printjson);
+
+var select = { id:1 }
+db.test.find(query, select).forEach(printjson);
