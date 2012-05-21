f0a3d3708a96bb9a3592ed24f8a1fde0ed31dbee
Rui Bando <bando.rui@gmail.com>
2012-05-22 00:51:36 +0900

    fix query.js : Add how to cursor

--- query.js
+++ query.js
@@ -1,3 +1,7 @@
+#-------------------------------------
+# ex.) Mongo Query by JavaScript
+# Author: Rui Bando
+#-------------------------------------
 //get explain
 var explain = db.test.find().explain();
 printjson(explain);
@@ -6,5 +10,14 @@ var query = { created_on : ISODate("2012-05-15T15:22:02.853Z") };
 //db.test.find(query).limit(10).forEach(function(){ print("name"); } );
 db.test.find(query).forEach(printjson);
 
+var where = "{ created_on : ISODate("2012-05-15T15:22:02.853Z") }, {id:1}"
+db.test.find(where).forEach(printjson);
+
 var select = { id:1 }
 db.test.find(query, select).forEach(printjson);
+
+//Use Cursor
+var query = db.test.find( { created_on : ISODate("2012-05-15T15:22:02.853Z") } );
+while (query.hasNext()){
+    printjson(query.next())
+};
